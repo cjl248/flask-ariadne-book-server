fe6e4796303f4457a727bab6a88799065acf01b7
github cjl248 <github cjl248@cornell.edu>
2021-01-14 20:58:40 -0500

    CORS settings

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 # run in command line: $ export FLASK_APP=main.py
 app = Flask(__name__)
+# Database connection
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.getcwd()}/ariadne-books-api.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+# CORS settings
+cors = CORS(app, resources={r"/graphql/*": {"origins": "*"}})
 
 db = SQLAlchemy(app)
 
